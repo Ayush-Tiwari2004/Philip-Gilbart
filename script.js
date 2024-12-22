ddba6cb7fe97c412cc0947fb88b99fe0eb646d39
Ayush-Tiwari2004 <ayushtiwari2004m@gmail.com>
2024-12-22 13:54:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let number = document.querySelector('.number');
+let counter = 0;
+
+setInterval(()=>{
+if(counter == 200){
+    clearInterval();
+}else{
+   counter += 1;
+   number.innerHTML=counter + '';
+}
+},.1);
+let numberd = document.querySelector('.numberd');
+let counterd = 0;
+
+setInterval(()=>{
+if(counterd == 353){
+    clearInterval();
+}else{
+   counterd += 1;
+   numberd.innerHTML=counterd + '';
+}
+},.1);
+let numbering = document.querySelector('.numbering');
+let countering = 20;
+
+setInterval(()=>{
+if(countering == 454){
+    clearInterval();
+}else{
+   countering += 1;
+   numbering.innerHTML=countering + '';
+}
+},.1);
+let numberr = document.querySelector('.numberr');
+let counterr = 0;
+
+setInterval(()=>{
+if(counterr == 500){
+    clearInterval();
+}else{
+   counterr += 1;
+   numberr.innerHTML=counterr + '';
+}
+},.1);
